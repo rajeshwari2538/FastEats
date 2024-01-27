61275720ed6337174b2890df6e06fce4beaa890b
rajeshwari2538 <rajeshwari2538@gmail.com>
2024-01-28 02:38:43 +0530

    styled header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM  from "react-dom/client";
-import Header from "./components/Header";
+import {Header} from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 import About from "./components/About";
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,5 +1,5 @@
 
-export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+export const LOGO_URL = "https://images-platform.99static.com/CDG3tPS1HqvP7u8kJysYXIf-os4=/500x500/top/smart/99designs-contests-attachments/58/58659/attachment_58659944";
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
