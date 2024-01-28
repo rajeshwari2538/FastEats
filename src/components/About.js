38b090eef3ad29f2fe0bf6bce52ab8344a8bb567
rajeshwari2538 <rajeshwari2538@gmail.com>
2024-01-28 12:01:24 +0530

    about + contact

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,87 @@
 
-
 const About = () => {
-    return(
-        <div>
-            <h1>About Us</h1>
-            <h2>This is Namaste React Web Series</h2>
-        </div>
-    )
-}
+ 
+  return (
+    <div className="min-h-screen text-left ">
+      <div class="container mx-auto my-8 p-8 bg-white shadow-lg  border border-gray-300 rounded-lg">
+        <h1 class="text-3xl font-bold mb-6">
+          🍴 FastEats: Amplify Your Dining Enjoyment
+        </h1>
 
+        <p class="text-lg mb-6">
+        FastEats, a sophisticated Swiggy mimic, transforms the food ordering experience with a user-friendly and feature-packed application.
+        </p>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div class="mb-6">
+            <h2 class="text-2xl font-bold mb-4">✨ Key Features:</h2>
+            <ul class="list-disc pl-6">
+              <li class="mb-2">
+                <span className="font-bold">Shimmer UI:</span> Immerse yourself in a visually stunning and
+                dynamic user interface, enhancing your overall experience.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Location-Aware Dining:</span> Leverages the live Swiggy API to
+                provide a personalized touch by displaying nearby restaurants,
+                featuring the best of Bangalore otherwise.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Search Restaurants:</span> Effortlessly find your favorite dining
+                spots with an intuitive search feature.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Top Rated Filters:</span>  Explore culinary excellence with the
+                ability to filter top-rated restaurants, ensuring a premium
+                dining experience.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Delicious Menus:</span> Dive into mouthwatering menus offered by
+                top restaurants, tantalizing your taste buds with every click.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Fully Responsive Design:</span> Whether on a smartphone, tablet, or
+                desktop, Munchmate adapts seamlessly to provide an optimal
+                viewing experience.
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 class="text-2xl font-bold mb-4">⚙️ Tech Marvels:</h2>
+            <ul class="list-disc pl-6">
+              <li class="mb-2">
+              <span className="font-bold">Built with React and TailwindCSS:</span> A blend of cutting-edge
+                technologies for a polished and modern UI.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Custom Hooks:</span> Elevate functionality with meticulously
+                crafted custom hooks, enhancing user engagement.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Redux-Toolkit for State Management:</span> Utilizes Redux-Toolkit
+                for efficient state management, ensuring a smooth and responsive
+                app.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Jest (Testing):</span> Rigorous testing with Jest ensures a robust
+                and bug-free application, guaranteeing a flawless user
+                experience.
+              </li>
+              <li class="mb-2">
+              <span className="font-bold">Parcel Bundling Magic:</span> Employs Parcel as the bundler,
+                optimizing performance and speeding up development.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <p class="text-lg mt-6">
+          🌟 **Experience the Culinary Revolution with FastEats!**
+        </p>
+      </div>
+
+    
+    </div>
+  );
+};
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,56 @@
 
-const Contact = () => {
-    return(
-        <div>
-            <h1>Contact</h1>
-        </div>
-    )
-}
+const ContactUs=()=>{
+    const details = {
+        name: 'Rajeshwari Gadagi',
+        bio: 'Frontend Developer',
+        contact: {
+          email: 'rajeshwarigadagi06@gmail.com',
+          github: 'https://github.com/rajeshwari2538',
+          linkedin: 'https://www.linkedin.com/in/rajeshwari-g-5bab95202/'
+        },
+      };
+    
+    
+    return (
+        
+            <div className='my-24 md:w-[600]  py-10 bg-gray-200 px-10 md:ml-[30%] md:mt-[8%] mt-[20%] w-[400] ml-[10%]'>
+              <h1 className='text-lg md:text-2xl my-4 font-bold'>Contact</h1>
+              <div>
+                <h2 className='text-md md:text-xl font-semibold'>Hi 👋, I'm {details.name} 👩‍💻</h2>
+                <p className='md:text-lg'>{details.bio}</p>
+        
+                <div className='my-4 space-y-2'>
+                  <h3 className='md:text-lg font-semibold'>Connect with me</h3>
+                  <p className='flex flex-wrap items-center gap-2'>
+                    <span className='font-semibold'>Gmail: </span>
+                    <a href={details.contact.email} className='flex items-center gap-1'>
+                      {details.contact.email}
+                    </a>
+                  </p>
+                  <p className='flex flex-wrap items-center gap-2'>
+                    <span className='font-semibold'>Github: </span>{' '}
+                    <a
+                      href={details.contact.github}
+                      className='flex items-center gap-1'
+                    >
+                      {details.contact.github}
+                    </a>
+                  </p>
+                  <p className='flex flex-wrap items-center gap-2'>
+                    <span className='font-semibold'>Linkedin: </span>{' '}
+                    <a
+                      href={details.contact.linkedin}
+                      className='flex items-center gap-1'
+                    >
+                      {details.contact.linkedin}
 
-export default Contact;
+                    </a>
+                  </p>
+                  
+                </div>
+              </div>
+            </div>
+          );
+    
+}
+export default ContactUs;
